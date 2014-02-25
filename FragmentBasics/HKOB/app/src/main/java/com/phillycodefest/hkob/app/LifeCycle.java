74325a63d57b959406b5e970f406de82b3b78713
Limao Luo <luolimao@gmail.com>
2014-02-25 15:09:15 -0500

    Jasdeep's code (maps api)

---
+++ FragmentBasics/HKOB/app/src/main/java/com/phillycodefest/hkob/app/LifeCycle.java
@@ -0,0 +1,53 @@
+package com.phillycodefest.hkob.app;
+
+import android.os.Bundle;
+import android.widget.Toast;
+/**
+ * Created by Jasdeep Glamour on 2/23/14.
+ */
+
+
+public class LifeCycle extends MainActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onPause() {
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+        super.onDestroy();
+    }
+}
