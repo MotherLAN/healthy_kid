4eab4d2724ed741a43be416642b96847d4314b46
meow <meow@meowmix.co>
2014-03-22 22:27:57 -0400

    removed unneeded properties, added and reorganized nutrients

--- MyApplication4/app/src/main/java/com/healthykid/android/Nutrient.java
+++ MyApplication4/app/src/main/java/com/healthykid/android/Nutrient.java
@@ -4,26 +4,31 @@ package com.healthykid.android;
  * An enum for all the nutrients
  */
 public enum Nutrient {
-    CALCIUM("calcium", R.string.calcium, "%"),
+    SERVINGS("servings", R.string.servings, ""),
 
     CALORIES("calories", R.string.calories, ""),
     FAT_CALORIES("fat_calories", R.string.fat_calories, ""),
 
+    FAT("total_fat", R.string.fat, "g"),
+    SATURATED_FAT("sat_fat", R.string.sat_fat, "g"),
+    MONOUNSATURATED_FAT("monounsat_fat", R.string.monounsat_fat, "g"),
+    POLYUNSATURATED_FAT("polyunsat_fat", R.string.polyunsat_fat, "g"),
+    TRANS_FAT("trans_fat", R.string.trans_fat, "g"),
 
     CHOLESTEROL("cholesterol", R.string.cholesterol, "mg"),
-    FIBER("dietary_fiber", R.string.fiber, "g"),
-    IRON("iron", R.string.iron, "%"),
-    POTASSIUM("potassium", R.string.potassium, "mg"),
-    PROTEIN("protein", R.string.protein, "g"),
-    SATURATED_FAT("sat_fat", R.string.sat_fat, "g"),
     SODIUM("sodium", R.string.sodium, "mg"),
-    SUGAR("sugars", R.string.sugar, "g"),
+    POTASSIUM("potassium", R.string.potassium, "mg"),
+
     CARBOHYDRATES("total_carb", R.string.carb, "g"),
-    FAT("total_fat", R.string.fat, "g"),
-    TRANS_FAT("trans_fat", R.string.trans_fat, "g"),
+    FIBER("dietary_fiber", R.string.fiber, "g"),
+    SUGAR("sugars", R.string.sugar, "g"),
+
+    PROTEIN("protein", R.string.protein, "g"),
+
     VITAMIN_A("vitamin_a", R.string.vitamin_a, "%"),
     VITAMIN_C("vitamin_c", R.string.vitamin_c, "%"),
-    SERVINGS("servings", R.string.servings, "");
+    CALCIUM("calcium", R.string.calcium, "%"),
+    IRON("iron", R.string.iron, "%");
 
     private final String request;
     private final int display;
